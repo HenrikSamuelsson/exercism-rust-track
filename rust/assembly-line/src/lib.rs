9b3a9eb49f8e6bb94f03408b5a195c3cb2b46869
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-18 09:23:06 +0200

    Add code to pass the failing test called production_rate_per_hour_at_speed_seven.

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,7 +3,14 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    speed as f64 * 221.0
+    let production_rate;
+    if speed < 5 {
+        production_rate = speed as f64 * 221.0;
+    }
+    else {
+        production_rate = speed as f64 * 221.0 * 0.9;
+    }
+    return production_rate;
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
