0319eebe3b48f12f4f748d1e46fc9fb0e1aa3a0c
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-18 11:05:14 +0200

    Add code to pass test production_rate_per_minute_at_speed_one.

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -17,5 +17,7 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    0
+    let items_count;
+    items_count = (production_rate_per_hour(speed) / 60.0) as u32;
+    return items_count;
 }
